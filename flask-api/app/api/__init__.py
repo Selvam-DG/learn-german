ef15e8ad7988654947a17f333403e4f4c74c4ecc
Selvam-DG <dasariselvam321@gmail.com>
2026-03-08 15:05:26 +0000

    feat(backend): add new routes for new UI features

--- flask-api/app/api/__init__.py
+++ flask-api/app/api/__init__.py
@@ -1,10 +1,12 @@
 from .health import bp as health_bp
-from .daily_vocab import bp as vocab_bp
+from .daily_vocab import bp as daily_vocab_bp
 from .admin.auth import bp as auth_bp
 from .admin.routes import bp as admin_bp
+from .vocab import vocab_bp
 
 def register_blueprints(app):
     app.register_blueprint(health_bp)
-    app.register_blueprint(vocab_bp)
+    app.register_blueprint(daily_vocab_bp)
     app.register_blueprint(auth_bp)
     app.register_blueprint(admin_bp)
+    app.register_blueprint(vocab_bp)
